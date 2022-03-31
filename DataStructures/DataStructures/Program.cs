2e4209b68793cb9b83a62e07405788e5c3b98fb3
Hermilo Ortega <hermilo.jh@gmail.com>
2022-03-30 21:01:41 -0700

    Update DataStructureProject with HashTables and HashSets

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -8,23 +8,25 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            //Dictionary<int, string> myDictionary = new Dictionary<int, string>();
-            //myDictionary.Add(1, "One");
-            //myDictionary.Add(2, "Two");
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+            myDictionary.Add(1, "One");
+            myDictionary.Add(2, "Two");
 
-            //Hashtable myHashtable = new Hashtable(myDictionary);
+            Hashtable myHashtable = new Hashtable(myDictionary);
 
-            //string valueOfOne = (string)myHashtable[1];
-            //Console.WriteLine($"Value of One = {valueOfOne}");
+            string valueOfOne = (string)myHashtable[1];
+            Console.WriteLine($"Value of One = {valueOfOne}");
 
-            //myHashtable.Remove(1);
+            myHashtable.Remove(1);
+            
 
-            //myHashtable.ContainsValue("One");
+            myHashtable.ContainsValue("One");
 
-            //foreach (DictionaryEntry value in myHashtable)
-            //{
-            //    Console.WriteLine($"Key: {value.Key}, value: {value.Value}");
-            //}
+            foreach (DictionaryEntry value in myHashtable)
+            {
+                Console.WriteLine($"Key: {value.Key}, value: {value.Value}");
+            }
+
 
 
             int[] nums = new int[4];
--- DataStructures/DataStructures/myHashSet.cs
+++ DataStructures/DataStructures/myHashSet.cs
@@ -6,14 +6,41 @@ namespace DataStructures
 {
     class myHashSet
     {
+        private Bucket[] bucketArray;
 
+        private int keyRange;
+
+        public myHashSet(){
+            this.keyRange = 769;
+            this.bucketArray = new Bucket[this.keyRange];
+            for (int i = 0; i < this.keyRange; i++){
+                this.bucketArray[i] = new Bucket();
+            }
+        }
+        protected int _hash(int key){
+            return (key % this.keyRange);
+        }
+
+        public void add(int key){
+            int bucketIndex = this._hash(key);
+            this.bucketArray[bucketIndex].insert(key);
+        }
+
+        public void remove(int key){
+            int bucketIndex = this._hash(key);
+            this.bucketArray[bucketIndex].delete(key);
+        }
+
+        public bool contains(int key){
+            int bucketIndex = this._hash(key);
+            return this.bucketArray[bucketIndex].exists(key);
+        }
     }
     class Bucket
     {
         private List<int> container;
 
-        public Bucket()
-        {
+        public Bucket(){
             this.container = new List<int>();
         }
         public void insert(int key){
@@ -25,8 +52,7 @@ namespace DataStructures
         public void delete(int key){
             this.container.Remove(key);
         }
-        public bool exists(int key)
-        {
+        public bool exists(int key){
             int index = this.container.IndexOf(key);
             return (index != -1);
         }
