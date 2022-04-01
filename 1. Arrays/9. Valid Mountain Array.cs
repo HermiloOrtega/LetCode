5d62e67ef6e7225d734483802c72e47f4927e3d2
Hermilo Ortega <hermilo.jh@gmail.com>
2022-03-31 20:49:53 -0700

    Update Arrays

--- 1. Arrays/9. Valid Mountain Array.cs
+++ 1. Arrays/9. Valid Mountain Array.cs
@@ -1,6 +1,5 @@
 // https://leetcode.com/problems/valid-mountain-array
 /*
-
 Given an array of integers arr, return true if and only if it is a valid mountain array.
 Recall that arr is a mountain array if and only if:
     arr.length >= 3
